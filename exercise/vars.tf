ac8858aed29668d6304d47ffe7364ebf525763fb
root <root@ip-172-31-80-164.ec2.internal>
2024-06-15 11:42:47 +0000

    pushing terraform exercise

---
+++ exercise/vars.tf
@@ -0,0 +1,15 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "S3BUCKET" {
+  default = "lavanyahyderabaddevopsclub2024"
+}
+
+variable "KEYNAME" {
+  default = "satrun"
+}
